803f7aca96b73e76c2966a6f720ad234bbf2501d
fofoiraq <fofoiraq23@gmail.com>
2018-04-18 14:31:10 -0600

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-    if (message.content.startsWith("$stats")) {
+    if (message.content.startsWith("-stats")) {
     message.channel.send({
         embed: new Discord.RichEmbed()
             .setColor('RANDOM')
@@ -25,7 +25,7 @@ client.on('message', message => {
             .addField('``Users rout``' ,`${client.users.size}` , true)
             .addField('``Name Bot Or tag``' , `${client.user.tag}` , true)
             .addField('``Bot Id``' , `${client.user.id}` , true)
-            .setFooter('SmiLeBoT / Team')
+            .setFooter('SpaceBot / Team')
     })
 }
 });
@@ -45,7 +45,7 @@ function timeCon(time) {
 
 client.on('message', message => {
     let args = message.content.split(' ').slice(1).join(' ');
-    if (message.content.startsWith('$bc-all')){
+    if (message.content.startsWith('-bc-all')){
     if(!message.author.id === '') return;
     message.channel.sendMessage('جار ارسال الرسالة :white_check_mark:')
     client.users.forEach(m =>{
