99422901408aee32aee408a52e0dd7ba6a89c51a
fofoiraq <fofoiraq23@gmail.com>
2018-06-03 16:44:42 -0600

    new code

--- bot.js
+++ bot.js
@@ -41,6 +41,40 @@ if (command == "embed") {
 
 });
 
+client.on('message', message => {
+    if (message.content.startsWith('+spotify')) {
+  let user = message.mentions.users.first() || message.author;
+    
+  
+  if (user.presence.activity !== null && user.presence.activity === 'LISTENING' && user.presence.activity.name === 'Spotify' && user.presence.activity.assets !== null) {
+  
+  let trackIMG = `https://i.scdn.co/image/${user.presence.activity.assets.largeImage.slice(8)}`;
+  let trackName = user.presence.activity.details;
+  let trackAuthor = user.presence.activity.state;
+  let trackAlbum = user.presence.activity.assets.largeText;
+  
+  
+  const embed = new Discord.RichEmbed()
+  .setAuthor('Spotify Track Info', 'https://cdn.discordapp.com/emojis/408668371039682560.png')
+    .setColor(0x1ED760)
+    .setThumbnail(trackIMG) 
+    .addField('Song Name', trackName, true)
+    .addField('Album', trackAlbum, true)
+    .addField('Author', trackAuthor, false) 
+    .addField('Listen to Track:', `[\`${trackURL}\`](trackURL)`, false); 
+      
+    message.channel.send({embed: embed})
+  } else {
+  if (!message.mentions.users.first()) {
+    message.channel.send('**You aren\'t listening to Spotify!**');
+  } else {
+    message.channel.send('**This user isn\'t listening to Spotify!**');
+  }
+  
+}
+}
+  });
+
 
 
 
